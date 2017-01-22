801fb7e6b736d29caea7f4008a69889bb75787df
Nikolay <Horushko.Nikolay@gmail.com>
2017-01-22 11:56:50 +0300

    optimize import, wrote javadoc

--- src/main/java/my/project/todolist/controller/servlets/ChangeTaskStatus.java
+++ src/main/java/my/project/todolist/controller/servlets/ChangeTaskStatus.java
@@ -3,17 +3,22 @@ package my.project.todolist.controller.servlets;
 import my.project.todolist.services.Service;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by Nikol on 1/21/2017.
  */
 public class ChangeTaskStatus extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /**
+     * The method accepts parameter taskId from request and change
+     * task status in the database on opposite.
+     * @param request
+     * @param response
+     * @throws ServletException
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         Service service = (Service) request.getServletContext().getAttribute("service");
         String parameter = request.getParameter("taskId");
         service.changeTaskStatus(Integer.parseInt(parameter));
