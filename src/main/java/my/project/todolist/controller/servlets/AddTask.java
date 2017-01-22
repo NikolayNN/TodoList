9ad2d870e0a351912e5cf58570d47fbda5469121
Nikolay <Horushko.Nikolay@gmail.com>
2017-01-22 11:48:29 +0300

    refactor and write javadoc

--- src/main/java/my/project/todolist/controller/servlets/AddTask.java
+++ src/main/java/my/project/todolist/controller/servlets/AddTask.java
@@ -3,6 +3,7 @@ package my.project.todolist.controller.servlets;
 import my.project.todolist.model.Task;
 import my.project.todolist.services.Service;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,12 +13,21 @@ import java.io.IOException;
  * Created by Nikol on 1/7/2017.
  */
 public class AddTask extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        Service service = (Service) request.getServletContext().getAttribute("service");
-        if(request.getParameter("taskDescription").length() == 0){
+    /**
+     * Accept from request parameter taskDescription check for empty and create
+     * new task in the database by service.
+     * if taskDescription is empty, will throw IllegalArgumentException.
+     * @param request
+     * @param response
+     * @throws ServletException
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String taskDescription = request.getParameter("taskDescription");
+        if(taskDescription.length() == 0){
             throw new IllegalArgumentException("Description can not be empty.");
         }
-        service.addTask(new Task(request.getParameter("taskDescription")));
+        Service service = (Service) request.getServletContext().getAttribute("service");
+        service.addTask(new Task(taskDescription));
     }
 
 
