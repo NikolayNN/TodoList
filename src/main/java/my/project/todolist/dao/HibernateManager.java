911ad463b95b313a1ad86dcfd3a0de4e9c88516f
Nikolay <Horushko.Nikolay@gmail.com>
2017-01-22 12:28:25 +0300

    refactor

--- src/main/java/my/project/todolist/dao/HibernateManager.java
+++ src/main/java/my/project/todolist/dao/HibernateManager.java
@@ -2,6 +2,7 @@ package my.project.todolist.dao;
 
 import my.project.todolist.model.Task;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
 import java.util.ArrayList;
@@ -11,18 +12,21 @@ import java.util.List;
  * Created by Nikol on 1/17/2017.
  */
 public class HibernateManager implements DatabaseManager {
+    private SessionFactory sessionFactory;
+
+    public HibernateManager() {
+        sessionFactory = HibernateSessionFactory.getSessionFactory();
+    }
 
     @Override
-    public void addTask(Task task){
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        session.beginTransaction();
+    public void addTask(Task task) {
+        Session session = sessionFactory.openSession();
         ItemEntity item = createItemFromTask(task);
         session.save(item);
-        session.getTransaction().commit();
         session.close();
     }
 
-    private ItemEntity createItemFromTask(Task task){
+    private ItemEntity createItemFromTask(Task task) {
         ItemEntity item = new ItemEntity();
         item.setDescription(task.getDescription());
         item.setDone(task.isDone());
@@ -30,38 +34,34 @@ public class HibernateManager implements DatabaseManager {
         return item;
     }
 
-    public List<Task> getTasksList(){
-        List<Task> result = new ArrayList<>();
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        session.beginTransaction();
+    @Override
+    public List<Task> getTasksList() {
+        Session session = sessionFactory.openSession();
         Query query = session.createQuery("from ItemEntity order by id");
-        session.getTransaction().commit();
-        List list = query.list();
-        for (Object o : list) {
-            Task task = createTaskFromItem((ItemEntity) o);
-            result.add(task);
-        }
+        List<Object> list = query.list();
         session.close();
-        return result;
+        return convertToTasksList(list);
     }
 
     @Override
     public List<Task> getNotCompletedTasksList() {
-        List<Task> result = new ArrayList<>();
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        session.beginTransaction();
+        Session session = sessionFactory.openSession();
         Query query = session.createQuery("from ItemEntity where done = false");
-        session.getTransaction().commit();
-        List list = query.list();
+        List<Object> list = query.list();
+        session.close();
+        return convertToTasksList(list);
+    }
+
+    private List<Task> convertToTasksList(List list) {
+        List<Task> result = new ArrayList<>();
         for (Object o : list) {
             Task task = createTaskFromItem((ItemEntity) o);
             result.add(task);
         }
-        session.close();
         return result;
     }
 
-    private Task createTaskFromItem(ItemEntity itemEntity){
+    private Task createTaskFromItem(ItemEntity itemEntity) {
         Task task = new Task(itemEntity.getDescription());
         task.setId(itemEntity.getId());
         task.setCreated(itemEntity.getCreated());
@@ -69,31 +69,29 @@ public class HibernateManager implements DatabaseManager {
         return task;
     }
 
-    public void close(){
-        HibernateSessionFactory.shutdown();
-    }
-
     @Override
     public void changeTaskStatus(int taskId) {
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        Session session = sessionFactory.openSession();
         session.beginTransaction();
-        Query query = session.createQuery("update ItemEntity set done = " + changedTaskStatus(taskId)  +
+        Query query = session.createQuery("update ItemEntity set done = " + changedTaskStatus(taskId) +
                 " where id = " + taskId);
-       query.executeUpdate();
+        query.executeUpdate();
         session.close();
     }
 
-    private boolean getIsDone(int taskId){
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from ItemEntity where id = " + taskId);
-        session.getTransaction().commit();
-        ItemEntity itemEntity = (ItemEntity) query.getSingleResult();
-        boolean bool = itemEntity.getDone();
-        return bool;
+    private boolean changedTaskStatus(int taskId) {
+        return !getIsDone(taskId);
     }
 
-    private boolean changedTaskStatus(int taskId){
-        return !getIsDone(taskId);
+    private boolean getIsDone(int taskId) {
+        Session session = sessionFactory.openSession();
+        Query query = session.createQuery("from ItemEntity where id = " + taskId);
+        ItemEntity itemEntity = (ItemEntity) query.getSingleResult();
+        return itemEntity.getDone();
+    }
+
+    @Override
+    public void close() {
+        HibernateSessionFactory.shutdown();
     }
 }
